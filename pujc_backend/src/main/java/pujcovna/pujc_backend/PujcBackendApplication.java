56c34cccbd50bfe0f76c2bb098c15277c3896240
elpharae <boura.frantisek01@gmail.com>
2023-11-08 14:41:17 +0100

    spring backend init

---
+++ pujc_backend/src/main/java/pujcovna/pujc_backend/PujcBackendApplication.java
@@ -0,0 +1,13 @@
+package pujcovna.pujc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PujcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PujcBackendApplication.class, args);
+	}
+
+}
---
+++ pujc_backend/src/test/java/pujcovna/pujc_backend/PujcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pujcovna.pujc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PujcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
