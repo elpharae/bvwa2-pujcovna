2a6d58e849f768d3b8d48c98381d4767dc4d8092
elpharae <boura.frantisek01@gmail.com>
2023-11-13 16:29:50 +0100

    gradle to maven

---
+++ pujcovna-kar/src/main/java/com/bvwa/pujcovnakar/PujcovnaKarApplication.java
@@ -0,0 +1,13 @@
+package com.bvwa.pujcovnakar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PujcovnaKarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PujcovnaKarApplication.class, args);
+	}
+
+}
